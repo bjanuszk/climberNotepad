231e7c504d055f8f9983b8adff319e8a376a1000
bjanuszkiewicz <bjanuszkiewicz@objectivity.co.uk>
2018-08-28 13:49:06 +0200

    Create project structure

---
+++ src/main/java/bj/climberNotepad/ClimberNotepadApplication.java
@@ -0,0 +1,12 @@
+package bj.climberNotepad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimberNotepadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimberNotepadApplication.class, args);
+	}
+}
